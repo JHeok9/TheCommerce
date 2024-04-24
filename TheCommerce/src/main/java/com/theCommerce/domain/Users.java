739109b73aed7fdc2f8a1fc6c7a36400e840a0eb
김준혁 <skdhd999@gmail.com>
2024-04-24 16:09:49 +0900

    User 도메인 작성

---
+++ TheCommerce/src/main/java/com/theCommerce/domain/Users.java
@@ -0,0 +1,85 @@
+package com.theCommerce.domain;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Users {
+
+	@Id
+	private String id;
+	private String pw;
+	private String nickname;
+	private String name;
+	private String phone;
+	private String email;
+	private Timestamp join_date;
+	
+	
+	public Users() {
+        this.join_date = new Timestamp(System.currentTimeMillis());
+    }
+
+	public String getId() {
+		return id;
+	}
+	
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	public String getPw() {
+		return pw;
+	}
+	
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	
+	public String getNickname() {
+		return nickname;
+	}
+	
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getPhone() {
+		return phone;
+	}
+	
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Timestamp getJoin_date() {
+		return join_date;
+	}
+
+	public void setJoin_date(Timestamp join_date) {
+		this.join_date = join_date;
+	}
+	
+	
+	
+}
