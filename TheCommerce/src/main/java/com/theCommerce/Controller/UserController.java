08c4f539f4b06057640284ab75228fa508c1c100
김준혁 <skdhd999@gmail.com>
2024-04-24 18:58:53 +0900

    회원가입 메소드 반환값 수정

--- TheCommerce/src/main/java/com/theCommerce/Controller/UserController.java
+++ TheCommerce/src/main/java/com/theCommerce/Controller/UserController.java
@@ -3,43 +3,54 @@ package com.theCommerce.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.theCommerce.domain.Users;
 import com.theCommerce.service.UserService;
 
 
 @Controller
+@RequestMapping("/api/user")
 public class UserController {
 	
-	private final UserService memberService;
+	private final UserService userService;
 
 	@Autowired
 	public UserController(UserService memberService) {
-		this.memberService = memberService;
+		this.userService = memberService;
 	}
 	
-	@GetMapping("/api/user/join")
+	// 회원가입 페이지 접근
+	@GetMapping("/join")
 	public String createForm() {
 		return "userJoinForm";
 	}
 	
-	@PostMapping("/api/user/join")
-	public String create(Users form) {
-		memberService.join(form);
-		
-		return "redirect:/";
-	}
+	// 회원가입 
+	@PostMapping("/join")
+    public ResponseEntity<String> joinUser(Users form) {
+        try {
+            String userId = userService.join(form);
+            return ResponseEntity.status(HttpStatus.CREATED).body("회원 가입이 성공적으로 완료되었습니다.");
+        } catch (IllegalStateException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
 	
-	@GetMapping("/api/user/list")
-	public String list(Model model) {
-		List<Users> userList = memberService.findUsers();
-		model.addAttribute("userList",userList);
+	// 회원목록 조회
+	@GetMapping("/list")
+	@ResponseBody
+	public List<Users> list() {
+		List<Users> userList = userService.findUsers();
 		
-		return "userList";
+		return userList;
 	}
 	
 }
