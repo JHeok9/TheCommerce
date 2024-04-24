19644d020519badf3016d87fb247288fc362e6b8
김준혁 <skdhd999@gmail.com>
2024-04-24 16:08:07 +0900

    데이터베이스 설정

---
+++ TheCommerce/sql/ddl.sql
@@ -0,0 +1,11 @@
+create table users
+(
+ id varchar(255) not null,
+ pw varchar(255) not null,
+ nickname varchar(255),
+ name varchar(255),
+ phone varchar(255),
+ email varchar(255),
+ join_date timestamp default current_timestamp,
+ primary key (id)
+);
