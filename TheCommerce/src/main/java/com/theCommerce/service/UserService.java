ad48a5ebe8d6ec8a1cbd7f93a529bf0833d4a863
김준혁 <skdhd999@gmail.com>
2024-04-24 16:11:58 +0900

    User 서비스 작성

---
+++ TheCommerce/src/main/java/com/theCommerce/service/UserService.java
@@ -0,0 +1,50 @@
+package com.theCommerce.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.theCommerce.domain.Users;
+import com.theCommerce.repository.UserRepository;
+
+
+@Transactional
+public class UserService {
+	
+	private UserRepository userRepository;
+	
+	public UserService(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+
+	// 회원가입
+	public String join(Users member) {
+		validateDuplicateMember(member); // 중복회원 검증
+		
+		userRepository.save(member);
+		return member.getId();
+	}
+
+	// 중복회원 검증
+	private void validateDuplicateMember(Users member) {
+		userRepository.findById(member.getId()).ifPresent(m -> {
+			throw new IllegalStateException("이미 존재하는 회원입니다");
+		});
+	}
+	
+	// 전체 회원 조회
+	public List<Users> findUsers(){
+		return userRepository.findAll();
+	}
+	
+	// 회원조회
+	public Optional<Users> findOne(String memberId){
+		return userRepository.findById(memberId);
+	}
+	
+	
+
+}
