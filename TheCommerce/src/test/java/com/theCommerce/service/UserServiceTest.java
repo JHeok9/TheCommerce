2f508dceda315edbe7781f9023955c3037071ca0
김준혁 <skdhd999@gmail.com>
2024-04-25 16:26:07 +0900

    회원가입 테스트 수정

--- TheCommerce/src/test/java/com/theCommerce/service/UserServiceTest.java
+++ TheCommerce/src/test/java/com/theCommerce/service/UserServiceTest.java
@@ -14,7 +14,7 @@ import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
-class userServiceTest {
+class UserServiceTest {
 
 	@Autowired UserService userService;
 	@Autowired UserRepository userRepository;
@@ -22,34 +22,39 @@ class userServiceTest {
 	@Test
 	void 회원가입() {
 		// given
-		Users user = new Users();
-		user.setId("abcd");
-		user.setPw("abcd");
-		
-		// when
-		String saveId = userService.join(user);
-		
-		// then
-		Users finduser = userService.findOne(saveId).get();
-		assertThat(user.getName()).isEqualTo(finduser.getName());
+	    Users user = new Users();
+	    user.setId("abcd");
+	    user.setPw("abcd");
+
+	    // when
+	    String result = userService.join(user);
+
+	    // then
+	    assertThat(result).isEqualTo("회원 가입이 성공적으로 완료되었습니다.");
 	}
 	
 	@Test
 	public void 중복_회원_예외() {
-		// given
+		// 회원 1
 		Users user1 = new Users();
 		user1.setId("abcd");
 		user1.setPw("abcd");
 
+		// 회원2
 		Users user2 = new Users();
 		user2.setId("abcd");
 		user2.setPw("abcd");
 		
-		// when
+		// 회원1 회원가입후 회원2 회원가입시도
 		userService.join(user1);
 		IllegalStateException e = assertThrows(IllegalStateException.class, () -> userService.join(user2));
 		
 		assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다");
+		
+	}
+	
+	@Test
+	public void 회원_목록_조회() {
 		
 	}
 }
